a8048b67290b88d7e70a5d535bcd9633e667f61e
BradenEverson <bradeneverson@gmail.com>
2024-11-05 04:51:47 -0600

    Add leader based remaps for all cargo commands

--- lua/bradeneverson/remap.lua
+++ lua/bradeneverson/remap.lua
@@ -8,13 +8,15 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Rust Based Remaps for builidng running and testing
-vim.keymap.set("n", "<C-b>", ":! cargo build<CR>")
-vim.keymap.set("n", "<C-b><C-r>", ":! cargo build -r<CR>")
+vim.keymap.set("n", "<leader>b", ":! cargo build<CR>")
+vim.keymap.set("n", "<leader>br", ":! cargo build -r<CR>")
 
-vim.keymap.set("n", "<C-t>", ":! cargo test<CR>")
-vim.keymap.set("n", "<C-t><C-r>", ":! cargo test -- --nocapture<CR>")
+vim.keymap.set("n", "<leader>t", ":terminal cargo test<CR>")
+vim.keymap.set("n", "<leader>tr", ":terminal cargo test -- --nocapture<CR>")
 
 vim.keymap.set("n", "<leader>r", ":terminal cargo run<CR>")
 vim.keymap.set("n", "<leader>rr", ":terminal cargo run -r<CR>")
+
+vim.keymap.set("n", "<leader>c", ":terminal cargo clippy")
 
 vim.keymap.set("n", "<leader>qq", ":bd!<CR>")
