c73150b6ddb7233224067d614f21891820b6ee80
Braden Everson <bradeneverson@gmail.com>
2025-01-09 17:01:59 -0600

    Add rebasing

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
-vim.keymap.set("n", "<leader>gr", ":Git restore .<CR>")
+vim.keymap.set("n", "<leader>grr", ":Git restore .<CR>")
+vim.keymap.set("n", "<leader>grb", ":Git rebase ")
 vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
