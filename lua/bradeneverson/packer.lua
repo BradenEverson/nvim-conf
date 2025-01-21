15ad30856564769538ae0d8acc8ea3b9b55f7c2f
BradenEverson <bradeneverson@gmail.com>
2025-01-21 15:08:49 -0600

    Add CSV view

--- lua/bradeneverson/packer.lua
+++ lua/bradeneverson/packer.lua
@@ -13,6 +13,27 @@ use {
 }
 
 use {
+    'cameron-wags/rainbow_csv.nvim',
+    config = function()
+        require 'rainbow_csv'.setup()
+    end,
+    -- optional lazy-loading below
+    module = {
+        'rainbow_csv',
+        'rainbow_csv.fns'
+    },
+    ft = {
+        'csv',
+        'tsv',
+        'csv_semicolon',
+        'csv_whitespace',
+        'csv_pipe',
+        'rfc_csv',
+        'rfc_semicolon'
+    }
+}
+
+use {
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
