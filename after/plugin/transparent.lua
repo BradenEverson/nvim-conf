6774ae0e66210a8b2dc1ed0afff7f51fd7caeae3
BradenEverson <bradeneverson@gmail.com>
2025-01-21 08:17:05 -0600

    Don't enable transparent by default

--- after/plugin/transparent.lua
+++ after/plugin/transparent.lua
@@ -8,5 +8,3 @@ require("transparent").setup({
   extra_groups = {}, -- table: additional groups that should be cleared
   exclude_groups = {}, -- table: groups you don't want to clear
 })
-
-vim.cmd("TransparentEnable")
