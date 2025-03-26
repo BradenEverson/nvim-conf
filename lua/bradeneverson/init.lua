a41ee5b96de9f8abed554c2a36b12693dc02e88e
BradenEverson <bradeneverson@gmail.com>
2025-03-26 05:33:37 -0500

    Add remaps for splitting and traversing windows

--- lua/bradeneverson/init.lua
+++ lua/bradeneverson/init.lua
@@ -1,3 +1,4 @@
 require("bradeneverson.remap")
 require("bradeneverson.set")
 require("bradeneverson.format")
+require("bradeneverson.split")
---
+++ lua/bradeneverson/split.lua
@@ -0,0 +1,7 @@
+vim.keymap.set("n", "<C-h>", ":winc h<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":winc j<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":winc k<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":winc l<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>sl", ":vsplit<CR>", { silent = true })
+vim.keymap.set("n", "<leader>sd", ":split<CR>", { silent = true })
