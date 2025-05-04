3bf9e6576725adb665fd9974b90ce1a025bfa2c9
BradenEverson <bradeneverson@gmail.com>
2025-05-04 07:39:38 -0500

    Move around telescope real nice

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,18 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+require('telescope').setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = require('telescope.actions').move_selection_next,
+        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+      },
+      n = {
+        ["<C-j>"] = require('telescope.actions').move_selection_next,
+        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+      },
+    },
+  },
+})
