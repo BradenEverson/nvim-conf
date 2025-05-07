0484ec3096e01f5a5d1457e848da3aa496bdfd6c
BradenEverson <bradeneverson@gmail.com>
2025-05-07 18:27:10 -0500

    Remove ensure installed

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,9 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
-lsp.ensure_installed({
-	'rust_analyzer'
-})
+-- lsp.ensure_installed({
+-- 	'rust_analyzer'
+-- })
 
 lsp.configure("rust_analyzer", {
     settings = {
